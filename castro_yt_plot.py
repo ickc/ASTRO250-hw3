6142623c36d3d6a0ef5bb1214cd6527aaa969b92
Kolen Cheung <christian.kolen@gmail.com>
2017-04-10 00:07:31 -0700

    castro_yt_plot.py: add batch mode

--- castro_yt_plot.py
+++ castro_yt_plot.py
@@ -12,15 +12,25 @@ def yt_plt(pltPath, annotate, ext):
     p1 = yt.ProjectionPlot(ds1, "z", "density")
     if annotate:
         p1.annotate_grids()
-    p1.save('.'.join((pltPath.rstrip(os.sep), ext)))
+    p1.save('.'.join((pltPath, ext)))
 
 def main(args):
     pltPath = args.f
     annotate = args.a
     ext = args.t
+    batchMode = args.b
 
     pltPath = os.path.expanduser(pltPath)
-    yt_plt(pltPath, annotate, ext)
+
+    if batchMode:
+        # get all the directories with name starting as plt
+        # these are the directories of each CASTRO plot output
+        pltDir = [name for name in os.listdir(pltPath)
+            if (os.path.isdir(os.path.join(pltsPath, name)) and name[:3] == "plt")]
+        for plt in pltDir:
+            yt_plt(os.path.join(pltsPath, plt), annotate, ext)
+    else:
+        yt_plt(pltPath.rstrip(os.sep), annotate, ext)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -28,6 +38,8 @@ if __name__ == "__main__":
     # Args
     parser.add_argument('-f',
                         help='The input directory of a single plot.')
+    parser.add_argument('-b',
+                        help='Batch mode: expect directories of name "plt..." within the directory specified with `-f`.')
     parser.add_argument('-a',
                         action='store_true',
                         help='Annotate the grids.')
