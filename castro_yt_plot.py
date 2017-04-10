7fad4b9e1a136f24e620b9abba6bcb217f0fef13
Kolen Cheung <christian.kolen@gmail.com>
2017-04-09 19:02:20 -0700

    castro_yt_plot.py: add argparse

--- castro_yt_plot.py
+++ castro_yt_plot.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python2
 
-import yt
+import argparse
 import os
+import yt
 
-def yt_plt(pltPath):
+def yt_plt(pltPath, annotate, ext):
     """Procedure to save the plots to pltPath.pdf
     """
     ds1= yt.load(pltPath)
     ds1.derived_field_list
     p1 = yt.ProjectionPlot(ds1, "z", "density")
-    p1.annotate_grids()
-    p1.save(pltPath + ".pdf")
+    if annotate:
+        p1.annotate_grids()
+    p1.save('.'.join((pltPath, ext)))
 
-if __name__ == "__main__":
-    pltsPath = os.path.expanduser("~/NERSC/Castro/Exec/hydro_tests/KH/")
+def main(args):
+    pltsPath = args.d
+    annotate = args.a
+    ext = args.t
+
+    pltsPath = os.path.expanduser(pltsPath)
     # get all the directories with name starting as plt
     # these are the directories of each CASTRO plot output
     pltDir = [name for name in os.listdir(pltsPath)
         if (os.path.isdir(os.path.join(pltsPath, name)) and name[:3] == "plt")]
     for plt in pltDir:
-        yt_plt(os.path.join(pltsPath, plt))
+        yt_plt(os.path.join(pltsPath, plt), annotate, ext)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(func=main)
+    # Args
+    parser.add_argument('-d',
+                        help='The input directory of the plots. Expects directories of name "plt..." within this directory.')
+    parser.add_argument('-a',
+                        action='store_true',
+                        help='Annotate the grids.')
+    parser.add_argument('-t',
+                        help='The output file extension. e.g. pdf, png, etc.')
+    args = parser.parse_args()
+    args.func(args)
